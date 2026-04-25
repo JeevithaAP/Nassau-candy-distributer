ad28165f3b492418d450850885d5cd0ea2baca33
Jeevitha A P <jeevithagowda340@gmail.com>
2026-04-25 15:52:23 +0530

    Update 5_Profit_Concentration.py

--- pages/5_Profit_Concentration.py
+++ pages/5_Profit_Concentration.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data_processing import (
     load_data,
