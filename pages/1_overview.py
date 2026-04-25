2eca3f2e7e2174dd002b672b19003afd1e10197b
Jeevitha A P <jeevithagowda340@gmail.com>
2026-04-25 15:49:42 +0530

    Update 1_overview.py

--- pages/1_overview.py
+++ pages/1_overview.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data_processing import (
     load_data,
